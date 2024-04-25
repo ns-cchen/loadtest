ed6dc6a39e4db04217447f0c5779c14ecf81dc17
Chien-Ku Chen <cchen@netskope.com>
2024-04-25 15:26:54 +0800

    refactor

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 )
 
 func main() {
-	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
-	duration := 10 * time.Second
+
 	targeter1 := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL:    "http://localhost:4567/search/apiv1/search?starttime=0&endtime=1714010845&sync=1&query=&limit=10&offset=0&tenantid=1016&trid=cchentest&explain=1&eventtype=alert",
@@ -33,21 +32,32 @@ func main() {
 		URL:    "http://localhost:4567/search/apiv1/search?starttime=0&endtime=1714010845&sync=1&query=&limit=10&offset=0&tenantid=1016&trid=cchentest&explain=1&collection=watchlist_events",
 	})
 	targeters := []vegeta.Targeter{targeter1, targeter2, targeter3, targeter4, targeter5}
+
+	attack(500, 60*time.Second, targeters)
+	attack(1000, 60*time.Second, targeters)
+	attack(2000, 60*time.Second, targeters)
+}
+
+func attack(frequency int, duration time.Duration, targeters []vegeta.Targeter) {
+	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+	rate := vegeta.Rate{Freq: frequency, Per: time.Second}
 	attacker := vegeta.NewAttacker()
 
 	var metrics vegeta.Metrics
 
-	randomIndex := rand.Intn(len(targeters))
+	randomIndex := r.Intn(len(targeters))
 	for result := range attacker.Attack(targeters[randomIndex], rate, duration, "Big Bang!") {
 		metrics.Add(result)
 	}
 
 	metrics.Close()
 
-	marshal, err := json.Marshal(metrics)
+	marshal, err := json.Marshal(metrics.Latencies)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%v", string(marshal))
+	fmt.Printf("Duration: %s\n", duration)
+	fmt.Printf("Rate: %f (nanosecond)\n", metrics.Rate)
+	fmt.Printf("Latencies: %s (nanosecond)\n", string(marshal))
 }
